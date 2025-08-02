502de536229259ce3590373ecfad2a3c871e8d8d
Happy2018new <chenbo79800@163.com>
2025-08-02 09:12:39 +0800

    Minor enhance for form interact

--- server/interact.go
+++ server/interact.go
@@ -36,7 +36,7 @@ func NewInteract(conn *minecraft.Conn) *Interact {
 }
 
 // sendFormAndWaitResponse ..
-func (i *Interact) sendFormAndWaitResponse(minecraftForm form.MinecraftForm) (resp any, err error) {
+func (i *Interact) sendFormAndWaitResponse(minecraftForm form.MinecraftForm) (resp any, isUserCanel bool, err error) {
 	for {
 		i.waiter, i.downWaiter = context.WithCancel(context.Background())
 
@@ -45,52 +45,55 @@ func (i *Interact) sendFormAndWaitResponse(minecraftForm form.MinecraftForm) (re
 			FormData: []byte(minecraftForm.PackToJSON()),
 		})
 		if err != nil {
-			return nil, fmt.Errorf("SendFormAndWaitResponse: %v", err)
+			return nil, false, fmt.Errorf("SendFormAndWaitResponse: %v", err)
 		}
 		i.formID++
 
 		select {
 		case <-i.waiter.Done():
 		case <-i.conn.Context().Done():
-			return nil, fmt.Errorf("SendFormAndWaitResponse: Minecraft connection has been closed")
+			return nil, false, fmt.Errorf("SendFormAndWaitResponse: Minecraft connection has been closed")
 		}
 
 		if i.clientResp.FormID != i.formID-1 {
-			return nil, fmt.Errorf(
+			return nil, false, fmt.Errorf(
 				"SendFormAndWaitResponse: Form ID not match (server = %d, client = %d)",
 				i.formID-1, i.clientResp.FormID,
 			)
 		}
 
-		_, ok := i.clientResp.CancelReason.Value()
+		canelReason, ok := i.clientResp.CancelReason.Value()
 		if ok {
+			if canelReason == packet.ModalFormCancelReasonUserClosed {
+				return nil, true, nil
+			}
 			time.Sleep(time.Second / 20)
 			continue
 		}
 
 		resp, ok := i.clientResp.ResponseData.Value()
 		if !ok {
-			return nil, fmt.Errorf("SendFormAndWaitResponse: Response data is not exist")
+			return nil, false, fmt.Errorf("SendFormAndWaitResponse: Response data is not exist")
 		}
 
 		switch minecraftForm.ID() {
 		case form.FormTypeMessage:
 			if strings.TrimSuffix(string(resp), "\n") == "true" {
-				return true, nil
+				return true, false, nil
 			}
-			return false, nil
+			return false, false, nil
 		case form.FormTypeAction:
 			result, err := strconv.ParseInt(strings.TrimSuffix(string(resp), "\n"), 10, 32)
 			if err != nil {
-				return nil, fmt.Errorf("SendFormAndWaitResponse: %v", err)
+				return nil, false, fmt.Errorf("SendFormAndWaitResponse: %v", err)
 			}
-			return int32(result), nil
+			return int32(result), false, nil
 		case form.FormTypeModal:
 			var respList []any
 
 			err = json.Unmarshal(resp, &respList)
 			if err != nil {
-				return nil, fmt.Errorf("SendFormAndWaitResponse: %v", err)
+				return nil, false, fmt.Errorf("SendFormAndWaitResponse: %v", err)
 			}
 
 			result := make([]any, 0)
@@ -111,16 +114,16 @@ func (i *Interact) sendFormAndWaitResponse(minecraftForm form.MinecraftForm) (re
 				case form.ModalFormElementStepSlider:
 					result = append(result, int32(value.(float64)))
 				default:
-					return nil, fmt.Errorf(
+					return nil, false, fmt.Errorf(
 						"SendFormAndWaitResponse: Unknown modal form element type %T; f.Contents[%d] = %#v",
 						f.Contents[index], index, f.Contents[index],
 					)
 				}
 			}
 
-			return result, nil
+			return result, false, nil
 		default:
-			return nil, fmt.Errorf(
+			return nil, false, fmt.Errorf(
 				"SendFormAndWaitResponse: Unknown minecraft form type %T; minecraftForm = %#v",
 				minecraftForm, minecraftForm,
 			)
@@ -142,7 +145,9 @@ func (i *Interact) sendFormAndWaitResponse(minecraftForm form.MinecraftForm) (re
 //   - [form.ModalFormElementDropdown] -> int32
 //   - [form.ModalFormElementSlider] -> int32
 //   - [form.ModalFormElementStepSlider] -> int32
-func (i *Interact) SendFormAndWaitResponse(minecraftForm form.MinecraftForm) (resp any, err error) {
+//
+// isUserCanel 指示表单是否是由用户通过叉号 (×) 关闭的
+func (i *Interact) SendFormAndWaitResponse(minecraftForm form.MinecraftForm) (resp any, isUserCanel bool, err error) {
 	i.mu.Lock()
 	defer i.mu.Unlock()
 	return i.sendFormAndWaitResponse(minecraftForm)
